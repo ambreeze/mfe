babf2c86a0787ca51dc143b1b104f850c20d7250
Alex Breeze <81765533+ambreeze@users.noreply.github.com>
2022-01-06 18:29:08 -0600

    Add in auth app and add lazy loading

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,8 +1,9 @@
 import React from 'react';
-var ReactDOM = require('react-dom');
+import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
+// Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     const history = defaultHistory || createMemoryHistory({ initialEntries: [initialPath] });
 
@@ -15,6 +16,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     return {
         onParentNavigate({ pathname: nextPathname }) {
             const { pathname } = history.location;
+
             if (pathname !== nextPathname) {
                 history.push(nextPathname);
             }
@@ -22,11 +24,16 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     };
 };
 
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+    const devRoot = document.querySelector('#_auth-dev-root');
+
     if (devRoot) {
         mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };
